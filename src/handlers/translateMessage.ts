80476a77647c8b310aa4df1878960a559e949d43
The-LukeZ <contact.lukez@proton.me>
2026-01-04 05:43:23 +0100

    Enhance translation command by adding integration types for user and guild installations

--- src/handlers/translateMessage.ts
+++ src/handlers/translateMessage.ts
@@ -2,9 +2,11 @@ import { Command, Option } from "discord-hono";
 import { factory } from "../init.js";
 import { SourceLanguageCode, TargetLanguageCode } from "deepl-node";
 import { ackRequest, AllLanguages, SourceLanguages, TargetLanguages } from "../utils.js";
-import { ApplicationCommandType } from "discord-api-types/v10";
+import { ApplicationCommandType, ApplicationIntegrationType } from "discord-api-types/v10";
 
-const command = new Command("Translate to Client Language", "Translate a message using DeepL").type(ApplicationCommandType.Message);
+const command = new Command("Translate to Client Language", "Translate a message using DeepL")
+  .type(ApplicationCommandType.Message)
+  .integration_types(ApplicationIntegrationType.UserInstall, ApplicationIntegrationType.GuildInstall);
 
 export const commandTranslateMessage = factory.command(command, async (c) => {
   if (c.interaction.data.type !== ApplicationCommandType.Message) return ackRequest(); // Type guard
