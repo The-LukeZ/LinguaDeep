d8402d952d20a48f60e7a4c3e3fbdcfb11239715
The-LukeZ <contact.lukez@proton.me>
2026-01-04 18:40:18 +0100

    fix: Add user wiki button to help command response

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -2,6 +2,7 @@ import { Command } from "discord-hono";
 import { factory } from "../init.js";
 import { EphemeralFlag } from "../utils.js";
 import { ApplicationIntegrationType } from "discord-api-types/payloads/v10";
+import { ActionRowBuilder, ButtonBuilder } from "@discordjs/builders";
 
 export const commandHelp = factory.command(
   new Command("help", "Get help with using the bot").integration_types(
@@ -21,6 +22,11 @@ export const commandHelp = factory.command(
         "This bot is a BYOK (Bring Your Own Key) application, so make sure to set your DeepL API key with `/key set`.",
         "-# The key is stored securely (encrypted) in the database and only used to make requests to DeepL on your behalf.",
       ].join("\n"),
+      components: [
+        new ActionRowBuilder<ButtonBuilder>()
+          .addComponents(new ButtonBuilder().setURL("https://github.com/The-LukeZ/LinguaDeep/wiki").setStyle(5).setLabel("User Wiki"))
+          .toJSON(),
+      ],
       flags: EphemeralFlag,
     }),
 );
